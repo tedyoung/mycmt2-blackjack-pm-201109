791ca2eda1f03c9691391d6d61310c0040659829
Ted M. Young <tedyoung@gmail.com>
2020-11-09 12:54:54 -0800

    Extracted Card.display() to a new class ConsoleCard.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -2,7 +2,7 @@ package com.jitterted.ebp.blackjack;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class CardDisplayTest {
 
@@ -12,7 +12,7 @@ public class CardDisplayTest {
   public void displayTenAsString() throws Exception {
     Card card = new Card(DUMMY_SUIT, Rank.TEN);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
         .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│10       │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│       10│\u001B[1B\u001B[11D└─────────┘");
   }
 
@@ -20,7 +20,7 @@ public class CardDisplayTest {
   public void displayNonTenAsString() throws Exception {
     Card card = new Card(DUMMY_SUIT, Rank.QUEEN);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
         .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│Q        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        Q│\u001B[1B\u001B[11D└─────────┘");
   }
 }
