e812d79673350a73f47c8748091c73a5421adc68
Ted M. Young <tedyoung@gmail.com>
2020-11-11 11:00:20 -0800

    Baseline web app.

---
+++ src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack;
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BlackjackGameApplication {
+
+  public static void main(String[] args) {
+//		ConsoleGame.main(new String[]{});
+    SpringApplication.run(BlackjackGameApplication.class, args);
+  }
+
+  @Bean
+  public Game createGame() {
+    return new Game();
+  }
+
+}
